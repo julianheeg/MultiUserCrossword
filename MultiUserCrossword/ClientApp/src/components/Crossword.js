278d414e57f9e3d7fdb0ff339d98f3924d44f441
Julian Heeg <julian.p.heeg@gmail.com>
2022-05-18 17:21:58 +0200

    CrosswordController made accessible from front end

---
+++ MultiUserCrossword/ClientApp/src/components/Crossword.js
@@ -0,0 +1,51 @@
+﻿import React, { Component } from 'react';
+
+export class Crossword extends Component {
+    static displayName = Crossword.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { cells: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.getCrossword();
+    }
+
+    static renderCrossword(crossword) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <tbody>
+                    {crossword.map(cell =>
+                        <tr key={cell.date}>
+                            <td>{cell.date}</td>
+                            <td>{cell.temperatureC}</td>
+                            <td>{cell.temperatureF}</td>
+                            <td>{cell.summary}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Crossword.renderCrossword(this.state.cells);
+
+        return (
+            <div>
+                <h1 id="tabelLabel">Crossword</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async getCrossword() {
+        const response = await fetch('crossword');
+        const data = await response.json();
+        this.setState({ cells: data, loading: false });
+    }
+}
--- MultiUserCrossword/Controllers/CrosswordController.cs
+++ MultiUserCrossword/Controllers/CrosswordController.cs
@@ -7,8 +7,15 @@ namespace MultiUserCrossword.Controllers;
 [Route("[controller]")]
 public class CrosswordController : ControllerBase
 {
+    private readonly ILogger<CrosswordController> _logger;
+
+    public CrosswordController(ILogger<CrosswordController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
-    public Crossword GetHardCodedExample()
+    public Crossword Get()
     {
         var crossword = Crossword.GenerateHardCodedExample();
         return crossword;
