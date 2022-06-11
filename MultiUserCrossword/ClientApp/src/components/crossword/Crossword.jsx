512bc488c6524e3e05e5a759368c4bf02f215898
Julian Heeg <julian.p.heeg@gmail.com>
2022-06-11 12:36:31 +0200

    first letter of word is focused when clue is clicked, and focus is moved to next letter when a letter is put in

--- MultiUserCrossword/ClientApp/src/components/crossword/Crossword.jsx
+++ MultiUserCrossword/ClientApp/src/components/crossword/Crossword.jsx
@@ -9,12 +9,14 @@ export class Crossword extends Component {
         super(props);
         this.state = {
             grid: [],
-            active: new Set(),
+            activeWord: [],
+            activeCell: null,
             loading: true
         };
 
         this.highlightHorizontalWord = this.highlightHorizontalWord.bind(this);
         this.highlightVerticalWord = this.highlightVerticalWord.bind(this);
+        this.focusNextLetterAfterInput = this.focusNextLetterAfterInput.bind(this);
     }
 
     render() {
@@ -35,7 +37,8 @@ export class Crossword extends Component {
                                 {row.map((cell, columnIndex) => {
                                     let key = rowIndex + ',' + columnIndex;
                                     return cell.hasCharacter ?
-                                        <WhiteCell key={key} solutionCharacter={cell.solutionCharacter} activeInput={this.state.active.has(key)} onClick={() => this.highlightHorizontalWord(rowIndex, columnIndex)} /> :
+                                        <WhiteCell key={key} solutionCharacter={cell.solutionCharacter} onClick={() => this.highlightHorizontalWord(rowIndex, columnIndex)} activeWord={this.state.activeWord.includes(key)}
+                                            activeCell={this.state.activeCell == key} onValidInput={() => this.focusNextLetterAfterInput(key)} /> :
                                         <BlackCell key={key} clueAcross={cell.clueAcross} clueDown={cell.clueDown} onHorizontalClueClick={() => this.highlightHorizontalWord(rowIndex, columnIndex)} onVerticalClueClick={() => this.highlightVerticalWord(rowIndex, columnIndex)} />
                                 })
                                 }
@@ -43,57 +46,77 @@ export class Crossword extends Component {
                         )}
                     </tbody>
                 </table>
-                active: { this.state.active }
+                activeWord: {this.state.activeWord}
+                activeCell: {this.state.activeCell}
             </div>
         );
     }
 
     highlightHorizontalWord(rowIndex, columnIndex) {
-        console.log("highlightHorizontalWord: " + rowIndex + ", " + columnIndex);
-
-        let active = new Set();
+        let activeWord = [];
 
         let row = this.state.grid[rowIndex];
-        // current cell and cells with greater index
-        for (let i = columnIndex + 1; i < row.length; i++) {
-            if (!row[i].hasCharacter)
-                break;
-            active.add(rowIndex + ',' + i);
-        }
         // cells with smaller index
         for (let i = columnIndex; i >= 0; i--) {
             if (!row[i].hasCharacter)
                 break;
-            active.add(rowIndex + ',' + i);
+            activeWord.push(rowIndex + ',' + i);
         }
 
-        console.log(active);
+        // cells are in opposite order, so we reverse
+        activeWord = activeWord.reverse();
 
-        this.setState({ active: active });
+        // current cell and cells with greater index
+        for (let i = columnIndex + 1; i < row.length; i++) {
+            if (!row[i].hasCharacter)
+                break;
+            activeWord.push(rowIndex + ',' + i);
+        }
+
+        let activeCell = this.getActiveCellAfterHighlight(rowIndex, columnIndex, activeWord);
+
+        this.setState({ activeWord: activeWord, activeCell: activeCell });
     }
 
     highlightVerticalWord(rowIndex, columnIndex) {
-        console.log("highlightVerticalWord: " + rowIndex + ", " + columnIndex);
-
-        let active = new Set();
+        let activeWord = [];
         let grid = this.state.grid;
+
+        // cells with smaller index
+        for (let i = rowIndex; i >= 0; i--) {
+            if (!grid[i][columnIndex].hasCharacter)
+                break;
+            activeWord.push(i + ',' + columnIndex);
+        }
+
+        // cells are in opposite order, so we reverse
+        activeWord = activeWord.reverse();
 
         // current cell and cells with greater index
         for (let i = rowIndex + 1; i < grid.length; i++) {
             if (!grid[i][columnIndex].hasCharacter)
                 break;
-            active.add(i + ',' + columnIndex);
-        }
-        // cells with smaller index
-        for (let i = rowIndex; i >= 0; i--) {
-            if (!grid[i][columnIndex].hasCharacter)
-                break;
-            active.add(i + ',' + columnIndex);
+            activeWord.push(i + ',' + columnIndex);
         }
 
-        console.log(active);
+        let activeCell = this.getActiveCellAfterHighlight(rowIndex, columnIndex, activeWord);
 
-        this.setState({ active: active });
+        this.setState({ activeWord: activeWord, activeCell: activeCell });
+    }
+
+    getActiveCellAfterHighlight(rowIndex, columnIndex, activeWord) {
+        if (this.state.grid[rowIndex][columnIndex].hasCharacter)
+            return rowIndex + ',' + columnIndex;
+        else
+            return activeWord[0];
+    }
+
+    focusNextLetterAfterInput(key) {
+        let currentLetterInWord = this.state.activeWord.indexOf(key);
+        if (currentLetterInWord == this.state.activeWord.length - 1)
+            return;
+        let nextActiveCell = this.state.activeWord[currentLetterInWord + 1];
+        this.setState({ activeCell: nextActiveCell });
     }
 
     componentDidMount() {
--- MultiUserCrossword/ClientApp/src/components/crossword/WhiteCell.jsx
+++ MultiUserCrossword/ClientApp/src/components/crossword/WhiteCell.jsx
@@ -4,6 +4,8 @@ export default class WhiteCell extends Component {
     constructor(props) {
         super(props);
         this.state = { guessedCharacter: null };
+        this.characterInput = React.createRef();
+        this.validateInput = this.validateInput.bind(this);
     }
 
     render() {
@@ -12,13 +14,31 @@ export default class WhiteCell extends Component {
             character = '';
 
         let className = 'whiteCell';
-        if (this.props.activeInput)
+        if (this.props.activeWord)
             className += ' activeWord';
 
         return (
             <td className={className}>
-                <input defaultValue={character} onClick={this.props.onClick} />
+                <input ref={this.characterInput}
+                    defaultValue={character} pattern="[A-Z]" size="1" maxLength="1" autoComplete="off" onClick={this.props.onClick} onInput={this.validateInput}/>
             </td>
         );
+    }
+
+    validateInput() {
+        let input = this.characterInput.current;
+        let inputValue = input.value;
+        inputValue = inputValue.toUpperCase();
+        if (/[A-Z]/.test(inputValue)) {
+            input.value = inputValue;   // set to upper case if lower case was put in
+            this.props.onValidInput();
+        }
+        else
+            input.value = '';
+    }
+
+    componentDidUpdate() {
+        if (this.props.activeCell)
+            this.characterInput.current.focus();
     }
 }
