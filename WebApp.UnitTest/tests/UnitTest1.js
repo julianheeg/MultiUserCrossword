85c47144c5824f5b128a9fe005162275668eb519
Julian Heeg <julian.p.heeg@gmail.com>
2022-07-03 15:07:49 +0200

    initial test commit

---
+++ WebApp.UnitTest/tests/UnitTest1.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+describe('Test Suite 1', function () {
+    it("Test 1 - This shouldn't fail", function () {
+        //expect(true).toBeTruthy();
+    });
+
+    it('Test 2 - This should fail', function () {
+        expect(1 === 1).toBeTruthy(); // "This shouldn't fail"
+        expect(false).toBeTruthy();
+    });
+
+    it('Test 3', function () {
+        assert.ok(true, "This shouldn't fail");
+    })
+});
